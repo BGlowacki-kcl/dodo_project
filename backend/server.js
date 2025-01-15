fc98a8d96330cc3166d88756979d479c2d5c8808
Bartosz <bglowacki.kcl@gmail.com>
2025-01-15 20:55:55 +0000

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
+app.listen(port, () => {
+    connectDB();
+    console.log(`Server is running on: http://localhost:${port}`);
+});
