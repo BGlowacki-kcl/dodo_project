f558186675e1d18a27e9472ab3a80f4e86f1be3d
Tianzhi <houtianzhi2005@gmail.com>
2025-01-27 19:37:12 +0000

    Create initial dashboards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Box from '@mui/material/Box';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import LandingPage from './pages/LandingPage';
+import ApplicantDashboard from "./dashboards/ApplicantDashboard.jsx";
+import EmployerDashboard from "./dashboards/EmployerDashboard.jsx";
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
 			<Navbar />
 			<Routes>
 				<Route path="/" element={<LandingPage />} />
+				<Route path="/applicantdashboard" element={<ApplicantDashboard />} />
+				<Route path="/employerdashboard" element={<EmployerDashboard />} />
 				<Route path="/home" element={<HomePage />} />
 			</Routes>
 		</Box>
