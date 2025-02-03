c30ac2991f582541877129cf1e98bbd3dcc16b21
Jay Sandhu <jaysandhu1117@gmail.com>
2025-02-03 20:09:14 +0000

    made user jobs page look better and added some functionality

--- frontend/src/pages/user/UserApplicationsPage.jsx
+++ frontend/src/pages/user/UserApplicationsPage.jsx
@@ -24,7 +24,7 @@ function UserApplicationsPage() {
                 <p>Status: {app.status}</p>
                 <p>Submitted: {new Date(app.submittedAt).toLocaleString()}</p>
                 {/*LINK TO APPLICATION PAGE HERE */}
-                <a href={`/user/applications/${app._id}`} className="text-blue-500 hover:underline"> View Details </a>
+                <a href={`/user/applications/${app._id}`} className="mt-4 inline-block px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"> View Details </a>
                 </li>
             ))}
             </ul>
