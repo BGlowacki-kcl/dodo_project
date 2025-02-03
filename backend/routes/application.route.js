8fc38af4d576e358c18beed17d392fad8aab918b
Saad Abdeen <k23093538@kcl.ac.uk>
2025-02-03 19:55:22 +0000

    add some routes for application

---
+++ backend/routes/application.route.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import applicationController from '../controllers/application.controller';
+import { ExplainVerbosity } from 'mongodb';
+
+const router = express.Router();
+
+router.post('/apply', applicationController.apply);
+router.get('/applicant/:applicantId', applicationController.getApplication);
+router.delete('/cancel', applicationController.cancel);
+router.put('/:id/status', applicationController.updateApplicationStatus);
+
+router.get('/applicant/:applicantId', applicationController.getApplication);
+
+export default router;
+
