396fb8e84c6cab094cb23c7c222692ee2c13e3e3
Bartosz <bglowacki.kcl@gmail.com>
2025-02-05 23:23:04 +0000

    Fix auth issue, add simple pdf input page

---
+++ frontend/src/pages/addPdf.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function addPdf() {
+
+    
+
+  return (
+    <div>
+        <h1> Add pdf </h1>
+        <input type="file" id="pdfInput" accept="application/pdf" />
+    </div>
+  )
+}
+
+export default addPdf
