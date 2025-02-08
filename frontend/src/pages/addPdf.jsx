f97acd694b0fe50af34217dcb6698be311fbebb6
Bartosz <bglowacki.kcl@gmail.com>
2025-02-08 11:05:51 +0000

    Do chatbot api function, fix resume praser to text

--- frontend/src/pages/addPdf.jsx
+++ frontend/src/pages/addPdf.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import extractTextFromPDF from "../services/resume.service.js";
 
 function addPdf() {
+  const [text, setText] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  
+  const handleUpload = async (event) => {
+    const file = event.target.files[0];
+    if(!file) return;
 
-    
+    setLoading(true);
+
+    extractTextFromPDF(file, setText, setError);
+  }
 
   return (
     <div>
         <h1> Add pdf </h1>
-        <input type="file" id="pdfInput" accept="application/pdf" />
+        <input type="file" id="pdfInput" accept="application/pdf" onChange={handleUpload} />
+        {loading && <div>Loading...</div>}
+        {text && <div className='w-96 border-3 border-black'>{text}</div>}
     </div>
   )
 }
 
-export default addPdf
+export default addPdf;
