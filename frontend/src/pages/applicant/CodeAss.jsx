d1994a947fe4815673f6649b14f3dd0dcd05c54b
Bartosz <bglowacki.kcl@gmail.com>
2025-03-01 16:03:46 +0000

    Create code assessment for python testining

--- frontend/src/pages/applicant/CodeAss.jsx
+++ frontend/src/pages/applicant/CodeAss.jsx
@@ -3,20 +3,24 @@ import Editor from "@monaco-editor/react";
 import { assessmentService } from '../../services/assessment.service';
 
 const CodeAss = () => {
-  const [code, setCode] = useState("// Write your code here");
-  const [language, setLanguage] = useState("javascript");
+  const [code, setCode] = useState(`# Write a function that takes number x and y, then returns the sum of x and y
+
+def func(x, y):
+  # Write your code here
+  `);
+  const [language, setLanguage] = useState("python");
   const [output, setOutput] = useState("");
   
   const runCode = async () => {
     const response = await assessmentService.runCode(code, language);
-    setOutput(response.data.output);
+    setOutput(response.data.stdout);
   }
 
   return (
     <div>
       <select onChange={(e) => setLanguage(e.target.value)}>
-        <option value="javascript">JavaScript</option>
         <option value="python">Python</option>
+        <option value="javascript">JavaScript</option>
         <option value="cpp">C++</option>
       </select>
 
--- frontend/src/services/assessment.service.js
+++ frontend/src/services/assessment.service.js
@@ -1,6 +1,36 @@
 
 export const assessmentService = {
     async runCode(code, language){
+        const testCases = `
+testCases = [
+    {
+        "input": [1, 2],
+        "output": 3,
+    },
+    {
+        "input": [3, 4],
+        "output": 7,
+    },
+    {
+        "input": [5, 6],
+        "output": 11,
+    },
+]
+`;
+        const addToCode = `
+for index, testCase in enumerate(testCases):
+    args = testCase["input"]
+    expected_output = testCase["output"]
+    actual_output = func(*args)
+
+    if actual_output == expected_output:
+        print("Test " + str(index + 1) + ": Test PASSED")
+    else:
+        print("Test {}: Test FAILED - input: {}, expected output: {}, output received: {}".format(index + 1, testCase['input'], expected_output, actual_output))
+`;
+        code += testCases + addToCode;
+
+        console.log("Code: ", code, "...");
         const response = await fetch('/api/code', {
             method: 'POST',
             headers: {
@@ -8,13 +38,15 @@ export const assessmentService = {
                 //'Authorization': `Bearer ${idToken}`,
             },
             body: JSON.stringify({
-                code: code,
+                source_code: code,
                 language: language,
             })
         });
         if (!response.ok) {
             throw new Error("Failed to run code");
         }
-        return response.json();
+        const data = await response.json();
+        console.log("Code: ",data);
+        return data;
     }
 }  
