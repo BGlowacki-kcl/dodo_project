0bb75aa9a884c495227881a3a02f822f85d7baa4
Bartosz <bglowacki.kcl@gmail.com>
2025-03-06 09:05:18 +0000

    Add notification system, fix token expired problem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,47 +27,44 @@ import ApplicantDetails from './pages/employer/ApplicantDetails';
 
 function App() {
 	const routeConfig = [
+		// For unLogged users (landing and authorization pages)
 		{ path: '/signin', element: <SignInUp/>, roles: ['unLogged'] },
 		{ path: '/signup', element: <SignInUp/>, roles: ['unLogged'] },
-		{ path: '/', element: <LandingPage />, roles: ['unLogged'] },
-		{ path: '/dashboard', element: <Dashboard />, roles: ['jobSeeker', 'employer'] },
-		{ path: '/applicant-dashboard', element: <ApplicantDashboard />, roles: ['jobSeeker'] },
-		{ path: '/employer-dashboard', element: <EmployerDashboard />, roles: ['employer'] },
-		{ path: '/posts', element: <EmployerPosts />, roles: ['employer'] },
-		{ path: '/user/jobs', element: <UserJobsPage />, roles: ['jobSeeker'] },
-		{ path: '/user/applications', element: <UserApplicationsPage />, roles: ['jobSeeker'] },
-		{ path: '/user/applications/:appId', element: <SingleApplicationPage />, roles: ['jobSeeker'] },
-		{ path: '/addDetails', element: <AddDetails />, roles: ['jobSeeker', 'employer'] },
-		{ path: '/posts/new', element: <CreateJobPost />, roles: ['employer'] },
-		{ path: '/posts/edit/:id', element: <EditJobPost />, roles: ['employer'] },
-		{ path: '/swipe', element: <Swiping />, roles: ['jobSeeker'] },
 		{ path: '/employer-login', element: <EmployerLogin />, roles: ['unLogged'] },
+		
+		// Pages for any user
+		{ path: '/', element: <LandingPage />, roles: ['unLogged', 'employer', 'jobSeeker'] },
+		{ path: '/search-results', element: <SearchResults />, roles: ['jobSeeker', 'employer', 'unLogged'] },
+		
+		// Completing profile page, for logged users
+		{ path: '/addDetails', element: <AddDetails />, roles: ['jobSeeker', 'employer'] },
+		
+		// jobSeeker accessible pages
+		{ path: '/swipe', element: <Swiping />, roles: ['jobSeeker'] },
+		{ path: '/user/jobs', element: <UserJobsPage />, roles: ['jobSeeker'] },
 		{ path: '/codeassessment', element: <CodeAss />, roles: ['jobSeeker'] },
+		{ path: '/user/applications', element: <UserApplicationsPage />, roles: ['jobSeeker'] },
+		{ path: '/applicant-dashboard', element: <ApplicantDashboard />, roles: ['jobSeeker'] },
+		{ path: '/applicant/:applicantId', element: <ApplicantDetails />, roles: ['jobSeeker'] },
+		{ path: '/user/applications/:appId', element: <SingleApplicationPage />, roles: ['jobSeeker'] },
+		
+		// Employer accessible pages
+		{ path: '/posts', element: <EmployerPosts />, roles: ['employer'] },
+		{ path: '/posts/new', element: <CreateJobPost />, roles: ['employer'] },
+		{ path: '/applicants', element: <EmployerApplicants />, roles: ['employer'] },
+		{ path: '/employer/posts', element: <EmployerPosts />, roles: ['employer'] },
+		{ path: '/posts/edit/:id', element: <EditJobPost />, roles: ['employer'] },
+		{ path: '/employer-dashboard', element: <EmployerDashboard />, roles: ['employer'] },
+		{ path: '/employer/applicants/:jobId', element: <EmployerApplicants />, roles: ['employer'] },
+		
+		// Forbidden page, for any user entering forbidden url
 		{ path: '/forbidden', element: <Forbidden />, dontCheck: true },
-		{ path: '/applicants', element: <EmployerApplicants />, roles: ['employer'] }, 
 	];
 
 	return (
 		<Box className="bg-background min-h-screen">
 			<Navbar />
 			<Routes>
-				<Route path='/SignIn' element={<SignInUp mode="SignIn" />} />
-				<Route path='/SignUp' element={<SignInUp mode="SignUp" />} />
-				<Route path="/dashboard" element={<AuthGuard> <Dashboard /> </AuthGuard>} />
-				<Route path="/" element={<LandingPage />} />
-				<Route path="/applicant-dashboard" element={<ApplicantDashboard />} />
-				<Route path="/employer-dashboard" element={<EmployerDashboard />} />
-				<Route path = "/employer-login" element={<EmployerSignIn />} />
-				<Route path="/employer/posts" element={<EmployerPosts />} />
-				<Route path="/user/jobs" element={<UserJobsPage />} />
-				<Route path="/user/applications" element={<UserApplicationsPage />} />
-				<Route path="/user/applications/:appId" element={<SingleApplicationPage />} />
-				<Route path="/addDetails" element={<AddDetails />} />				
-				<Route path="/posts/new" element={<CreateJobPost />} />
-				<Route path="/posts/edit/:id" element={<EditJobPost />} />
-				<Route path="/swipe" element={<Swiping />} />
-				<Route path="/employer/applicants/:jobId" element={<EmployerApplicants />} />
-				<Route path="/applicant/:applicantId" element={<ApplicantDetails />} />
 				{routeConfig.map((route) => (
 					<Route
 						key={route.path}
@@ -81,8 +78,7 @@ function App() {
 						}
 					/>
 				))}
-				<Route path="/search-results" element={<SearchResults />} />
-				<Route path="/addPdf" element={<AddPdf />} />
+
 			</Routes>
 		</Box>
 	);
