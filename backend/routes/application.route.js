1e0a5f50e1a33f61dbc5c8b7601a95abad922a16
Saad Abdeen <saad.a.abdeen@gmail.com>
2025-03-20 13:42:30 +0000

    revert

--- backend/routes/application.route.js
+++ backend/routes/application.route.js
@@ -12,5 +12,6 @@ router.get("/all", checkRole(["jobSeeker"]), applicationController.getAllApplica
 router.get("/byId", checkRole(["jobSeeker", "employer"]), applicationController.getOneApplication);
 router.post("/apply", checkRole(["jobSeeker"]), applicationController.createApplication);
 router.delete("/withdraw", checkRole(["jobSeeker", "employer"]), applicationController.withdrawApplication);
+router.get("/job/:jobId/applicants", checkRole(["employer"]), applicationController.getApplicants);
 
 export default router;
