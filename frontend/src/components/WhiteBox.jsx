f4f48b5796e5c04abd3b36ac60fbfa47dc6bc6ec
Saad Abdeen <saad.a.abdeen@gmail.com>
2025-03-23 02:40:09 +0000

    remove comment

--- frontend/src/components/WhiteBox.jsx
+++ frontend/src/components/WhiteBox.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const WhiteBox = ({ children, className }) => (
-  <div className={`bg-white p-6 rounded-xl shadow-sm ${className}`}> {/* Reduced padding from p-6 to p-4 */}
+  <div className={`bg-white p-6 rounded-xl shadow-sm ${className}`}> 
     {children}
   </div>
 );
